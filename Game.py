6b2abce6bc2a660caaa81f5ca0c1a20031253a0d
riley skinner <rskime13@kearsarge.org>
2014-12-08 11:30:39 -0500

    more wall

--- Game.py
+++ Game.py
@@ -1,5 +1,5 @@
 import pygame, sys, random
-
+from Wall import Wall
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -14,13 +14,21 @@ screen = pygame.display.set_mode(size)
 bgImage = pygame.image.load("Resources/Objects/Backgrounds/dungeon are lvl 1.PNG")
 bgRect = bgImage.get_rect()
 
+walls = [Wall([0,0],[338,68]),
+		 Wall([48,133],[145,164]),
+		 Wall([0,68],[48,230])]
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
 		
-		
 	bgColor = r,g,b
 	screen.fill(bgColor)
 	screen.blit(bgImage, bgRect)
+	for wall in walls:
+		screen.blit(wall.image, wall.rect)
 	pygame.display.flip()
 	clock.tick(60)
+bgColor = r,g,b
+
+		
