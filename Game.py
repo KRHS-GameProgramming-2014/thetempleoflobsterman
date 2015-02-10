f1b2c9302b30fcb085dd247c3f6eb66a16166523
riley <riley.skinner@kearsarge.org>
2015-02-10 10:44:20 -0500

    player moves

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 import pygame, sys, random
 from Wall import Wall
+from Player import Player
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -15,33 +16,59 @@ bgImage = pygame.image.load("Resources/Objects/Backgrounds/dungeon are lvl 1.PNG
 bgRect = bgImage.get_rect()
 
 walls = [Wall([0,0],[338,68]),
-		 Wall([48,133],[145,164]),
-		 Wall([0,68],[48,230]),
-		 Wall([0,230],[15, 260]),
-		 Wall([0,260],[48,611]),
-		 Wall([242,326],[302,386]),
-		 Wall([45,355],[205,386]), 
-		 Wall([0,615],[978,648]),
-		 Wall([947,0],[957,543]),
-		 Wall([273,132],[366,163]),
-		 Wall([337,163],[364,290]),
-		 Wall([782,228],[910,257]), 
-		 ]
-		 
-		 
-		 
+         Wall([48,133],[145,164]),
+         Wall([0,68],[48,230]),
+         Wall([0,230],[15, 260]),
+         Wall([0,260],[48,611]),
+         Wall([242,326],[302,386]),
+         Wall([45,355],[205,386]), 
+         Wall([0,615],[978,648]),
+         Wall([947,0],[957,543]),
+         Wall([273,132],[366,163]),
+         Wall([337,163],[364,290]),
+         Wall([782,228],[910,257]), 
+         Wall([434,36],[465,98]),
+         Wall([337,0],[972,32]),
+         Wall([462,70],[497,230]),
+         ]
+         
+player = Player([25,245])        
+         #100 #100
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
-		
-	bgColor = r,g,b
-	screen.fill(bgColor)
-	screen.blit(bgImage, bgRect)
-	for wall in walls:
-		screen.blit(wall.image, wall.rect)
-	pygame.display.flip()
-	clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                player.go("up")
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.go("right")
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                player.go("down")
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.go("left")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                player.go("stop up")
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.go("stop right")
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                player.go("stop down")
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.go("stop left")
+    player.update(width, height)
+    
+    for wall in walls:
+        player.collideWall(wall)
+    
+    bgColor = r,g,b
+    screen.fill(bgColor)
+    screen.blit(bgImage, bgRect)
+    for wall in walls:
+        screen.blit(wall.image, wall.rect)
+    screen.blit(player.image, player.rect)
+    pygame.display.flip()
+    clock.tick(60)
 bgColor = r,g,b
 
-		
+        
--- Player.py
+++ Player.py
@@ -1,1 +1,154 @@
+import pygame
 
+class Player():
+    def __init__(self, pos):      
+        self.upImages = [pygame.image.load("Resources/Objects/Player/Briansteelup1.PNG"),
+                         pygame.image.load("Resources/Objects/Player/Briansteelup2.PNG"),
+                         pygame.image.load("Resources/Objects/Player/Briansteelup3.PNG")]
+        self.downImages = [pygame.image.load("Resources/Objects/Player/Briansteeldown1.PNG"),
+                           pygame.image.load("Resources/Objects/Player/Briansteeldown2.PNG"),
+                           pygame.image.load("Resources/Objects/Player/Briansteeldown3.PNG")]
+        self.leftImages = [pygame.image.load("Resources/Objects/Player/Briansteelleft1.PNG"),
+                           pygame.image.load("Resources/Objects/Player/Briansteelleft2.PNG"),
+                           pygame.image.load("Resources/Objects/Player/Briansteelleft3.PNG")]
+        self.rightImages = [pygame.image.load("Resources/Objects/Player/Briansteelright1.PNG"),
+                            pygame.image.load("Resources/Objects/Player/Briansteelright2.PNG"),
+                            pygame.image.load("Resources/Objects/Player/Briansteelright3.PNG")]
+        self.upImages = [pygame.image.load("Resources/Objects/Player/Briansteelup1.PNG"),
+                            pygame.image.load("Resources/Objects/Player/Briansteelup2.PNG"),
+                            pygame.image.load("Resources/Objects/Player/Briansteelup3.PNG")]
+        self.control = [pygame.image.load("Resources/Objects/Player/capture.PNG"),
+                            pygame.image.load("Resources/Objects/Player/capture3.PNG"),
+                            pygame.image.load("Resources/Objects/Player/capture4.PNG")]                  
+        self.heart = [pygame.image.load("Resources/Objects/Player/emptyheart.PNG"),
+                            pygame.image.load("Resources/Objects/Player/halfheart.PNG"),
+                            pygame.image.load("Resources/Objects/Player/heart.PNG"),
+                            pygame.image.load("Resources/Objects/Player/onethirdheart.PNG")]
+        self.pain = [pygame.image.load("Resources/Objects/Player/playerpaindown.PNG"),
+                            pygame.image.load("Resources/Objects/Player/playerpainleft.PNG"),
+                            pygame.image.load("Resources/Objects/Player/playerpainright.PNG"),
+                            pygame.image.load("Resources/Objects/Player/playerpainup.PNG")]                  
+        self.run = [pygame.image.load("Resources/Objects/Player/runn.PNG")]
+        self.stabdown = [pygame.image.load("Resources/Objects/Player/stabdown.PNG"),
+                            pygame.image.load("Resources/Objects/Player/stabdown2.PNG"),
+                            pygame.image.load("Resources/Objects/Player/stabdown3.PNG")]
+        self.stableft = [pygame.image.load("Resources/Objects/Player/stableft1.PNG"),
+                            pygame.image.load("Resources/Objects/Player/stableft2.PNG"),
+                            pygame.image.load("Resources/Objects/Player/stableft3.PNG")]
+        self.stabright = [pygame.image.load("Resources/Objects/Player/stabright1.PNG"),
+                            pygame.image.load("Resources/Objects/Player/stabright2.PNG"),
+                            pygame.image.load("Resources/Objects/Player/stabright3.PNG")]
+        self.stabup = [pygame.image.load("Resources/Objects/Player/stabup.PNG"),
+                            pygame.image.load("Resources/Objects/Player/stabup2.PNG"),
+                            pygame.image.load("Resources/Objects/Player/stabup3.PNG")]
+        self.knifethrowing = [pygame.image.load("Resources/Objects/Player/throwing knife.PNG")]                                         
+        self.facing = "up"
+        self.changed = False
+        self.images = self.upImages
+        self.frame = 0
+        self.maxFrame = len(self.images) - 1
+        self.waitCount = 0
+        self.maxWait = 60*.25
+        self.image = self.images[self.frame]
+        self.rect = self.image.get_rect(center = pos)
+        self.maxSpeed = 5  
+        self.speedx = 0
+        self.speedy = 0
+        self.speed = [self.speedx, self.speedy]             
+
+    def update(self, width, height):
+        self.didBounceX = False
+        self.didBounceY = False
+        self.move()
+        self.collideEdge(width, height)
+        self.animate()
+        self.changed = False
+    
+    def move(self):
+        self.speed = [self.speedx, self.speedy]
+        self.rect = self.rect.move(self.speed)
+        
+    def collideEdge(self, width, height):
+        if not self.didBounceX:
+            #print "trying to hit Wall"
+            if self.rect.left < 0 or self.rect.right > width:
+                self.didBounceX = True
+                self.speedx = 0
+                #print "hit xWall"
+        if not self.didBounceY:
+            if self.rect.top < 0 or self.rect.bottom > height:
+                self.didBounceY = True
+                self.speedx = 0
+                #print "hit xWall"
+    
+    def animate(self):
+        if self.waitCount < self.maxWait:
+            self.waitCount += 1
+        else:
+            self.waitCount = 0
+            self.changed = True
+            if self.frame < self.maxFrame:
+                self.frame += 1
+            else:
+                self.frame = 0
+        
+        if self.changed:    
+            if self.facing == "up":
+                self.images = self.upImages
+            elif self.facing == "down":
+                self.images = self.downImages
+            elif self.facing == "right":
+                self.images = self.rightImages
+            elif self.facing == "left":
+                self.images = self.leftImages
+            
+            self.image = self.images[self.frame]
+    
+    def collideWall(self, other):
+        if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
+            if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
+                if not self.didBounceX:
+                    self.speedx = -self.speedx
+                    self.move()
+                    self.speedx = 0
+                    self.didBouncex = True
+                if not self.didBounceY:
+                    self.speedy = -self.speedy
+                    self.move()
+                    self.speedy = 0
+                    self.didBounceY = True
+                    #print "hit Ball"
+    
+    def go(self, direction):
+        if direction == "up":
+            self.facing = "up"
+            self.changed = True
+            self.speedy = -self.maxSpeed
+        elif direction == "stop up":
+            self.speedy = 0
+        elif direction == "down":
+            self.facing = "down"
+            self.changed = True
+            self.speedy = self.maxSpeed
+        elif direction == "stop down":
+            self.speedy = 0
+            
+        if direction == "right":
+            self.facing = "right"
+            self.changed = True
+            self.speedx = self.maxSpeed
+        elif direction == "stop right":
+            self.speedx = 0
+        elif direction == "left":
+            self.facing = "left"
+            self.changed = True
+            self.speedx = -self.maxSpeed
+        elif direction == "stop left":
+            self.speedx = 0
+
+    def distance(self, pt):
+        x1 = self.rect.center[0]
+        y1 = self.rect.center[1]
+        x2 = pt[0]
+        y2 = pt[1]
+        return math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
