40bbe2caa32b3a7438e85856309b709c3afcdf0c
brian <brian.callaghan@kearsarge.org>
2015-03-04 08:43:10 -0500

    player code

--- Player.py
+++ Player.py
@@ -142,7 +142,7 @@ class Player():
             self.facing = "left"
             self.changed = True
             self.speedx = -self.maxSpeed
-7            self.speedx = 0
+            self.speedx = 0
 
     def distance(self, pt):
         x1 = self.rect.center[0]
